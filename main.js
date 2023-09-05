e4176c904d2fbe05ad13e6184bdae4c1b3fe903f
dvhi <131928785+dvhi@users.noreply.github.com>
2023-09-04 18:55:48 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload(){
+
+}
+
+function setup(){
+   var canvas = createCanvas(400, 500); 
+   canvas.center();
+}
+
+function draw(){
+
+}
+
+function take_snapshot()
+{
+    save('myFilterImage.png');
+}
+
